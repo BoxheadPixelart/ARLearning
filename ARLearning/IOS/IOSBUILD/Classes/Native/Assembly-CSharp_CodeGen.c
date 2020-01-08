6e76259a47050cd0177613ccd4b953b6cb181f24
Storm hughes <boxheadpixelart@gmail.com>
2020-01-08 16:36:03 -0600

    Project Setup

---
+++ ARLearning/IOS/IOSBUILD/Classes/Native/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,55 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void TapToPlace::Start()
+extern void TapToPlace_Start_m43B0028354F221121438A54F52FC2D25D350E8B5 ();
+// 0x00000002 System.Void TapToPlace::Update()
+extern void TapToPlace_Update_mE6AF20E9CC5137BB575093ABC52D42E6490E160A ();
+// 0x00000003 System.Void TapToPlace::.ctor()
+extern void TapToPlace__ctor_m0E3BD5F22A552D0B2B61B45A07F54343F483D643 ();
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	TapToPlace_Start_m43B0028354F221121438A54F52FC2D25D350E8B5,
+	TapToPlace_Update_mE6AF20E9CC5137BB575093ABC52D42E6490E160A,
+	TapToPlace__ctor_m0E3BD5F22A552D0B2B61B45A07F54343F483D643,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
